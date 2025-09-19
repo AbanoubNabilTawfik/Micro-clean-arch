024f411a542fd029ceb1f5d77cc587913ebb7d2a
Abanoub Mansour <amansour@ecapital.com>
2025-09-19 16:07:17 +0300

    commit changes

--- services/catalog/Catalog.Core/obj/Debug/net9.0/Catalog.Core.AssemblyInfo.cs
+++ services/catalog/Catalog.Core/obj/Debug/net9.0/Catalog.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af47835c0246a62885c726c7e56984eb80b30212")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
